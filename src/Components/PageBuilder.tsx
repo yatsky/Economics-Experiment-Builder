5be3a2f556afc073b5e0d721fd4708e6a0fef70c
Yaoni <wyatsky@gmail.com>
2020-06-13 22:55:55 +1000

    Prettify PageBuilder.

--- src/Components/PageBuilder.tsx
+++ src/Components/PageBuilder.tsx
@@ -2,33 +2,33 @@ import React, { ReactPropTypes } from 'react';
 import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
 import Paper from '@material-ui/core/Paper';
-import {makeStyles, createStyles, Theme} from '@material-ui/core/styles';
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
 import { PropTypes } from '@material-ui/core';
 import PageElement from './PageElement';
 
 const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      flexGrow: 1,
-    },
-    paper: {
-      padding: theme.spacing(2),
-      textAlign: 'center',
-      color: theme.palette.text.secondary,
-    },
-  }),
+    createStyles({
+        root: {
+            flexGrow: 1,
+        },
+        paper: {
+            padding: theme.spacing(2),
+            textAlign: 'center',
+            color: theme.palette.text.secondary,
+        },
+    }),
 );
 export default function PageBuilder() {
-        const classes = useStyles();
-        return (
-            <Box border={1}>
-                <Grid container>
-                    <Grid item xs={12}>
-                        <Paper className={classes.paper}>
+    const classes = useStyles();
+    return (
+        <Box border={1}>
+            <Grid container>
+                <Grid item xs={12}>
+                    <Paper className={classes.paper}>
                         <PageElement />
-                        </Paper>
-                    </Grid>
+                    </Paper>
                 </Grid>
-            </Box>
-        )
+            </Grid>
+        </Box>
+    )
 }
