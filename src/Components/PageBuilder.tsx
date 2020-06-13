3fb802a1bee4756c3646636f947f1883570791f2
Yaoni <wyatsky@gmail.com>
2020-06-13 20:50:52 +1000

    Add PageBuilder

---
+++ src/Components/PageBuilder.tsx
@@ -0,0 +1,31 @@
+import React, { ReactPropTypes } from 'react';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import Paper from '@material-ui/core/Paper';
+import {makeStyles, createStyles, Theme} from '@material-ui/core/styles';
+import { PropTypes } from '@material-ui/core';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1,
+    },
+    paper: {
+      padding: theme.spacing(2),
+      textAlign: 'center',
+      color: theme.palette.text.secondary,
+    },
+  }),
+);
+export default function PageBuilder(props: {className: string}) {
+        const classes = useStyles();
+        return (
+            <Box border={1} className={props.className}>
+                <Grid container>
+                    <Grid item xs={12}>
+                        <Paper className={classes.paper}>Dummy</Paper>
+                    </Grid>
+                </Grid>
+            </Box>
+        )
+}
