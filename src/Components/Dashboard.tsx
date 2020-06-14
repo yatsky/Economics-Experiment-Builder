2e605827f62ae68798af92ec463e0788482c7425
Yaoni <wyatsky@gmail.com>
2020-06-15 09:30:19 +1000

    Fix Dashboard export name.

--- src/Components/Dashboard.tsx
+++ src/Components/Dashboard.tsx
@@ -46,7 +46,7 @@ const useStyles = makeStyles((theme: Theme) =>
     }),
 );
 
-export default function ButtonAppBar() {
+export default function Dashboard() {
     const classes = useStyles();
 
     return (
